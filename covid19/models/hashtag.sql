af1f7df847bd1fc455318934dc5c059c0a735e2c
Sotatek-HaiHo <hai.ho@sotatek>
2023-04-07 10:18:04 +0700

    [COVID19] Update

--- covid19/models/hashtag.sql
+++ covid19/models/hashtag.sql
@@ -8,7 +8,12 @@
 }}
 
 with hst as (SELECT distinct user_id, tweet_date, unnest(regexp_matches(lower(content), '#\w+', 'g')) AS hashtag
-             FROM {{ source('raw_tweets', 'raw_tweets') }})
+             FROM {{ source('raw_tweets', 'raw_tweets') }}),
+     hst2 as (SELECT user_id, tweet_date, hashtag
+              FROM hst)
 
-SELECT distinct user_id, tweet_date, hashtag
-FROM hst
+select user_id,
+       tweet_date,
+       array_agg(hashtag) AS hastags
+from hst2
+group by user_id, tweet_date
