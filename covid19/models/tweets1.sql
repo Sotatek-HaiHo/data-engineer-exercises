18725a4ec46975a578d7d0e9c9b6a58f2e23b666
Sotatek-HaiHo <hai.ho@sotatek>
2023-04-07 09:34:21 +0700

    [COVID19] Update

--- covid19/models/tweets1.sql
+++ covid19/models/tweets1.sql
@@ -16,4 +16,4 @@ FROM {{ source('raw_tweets', 'raw_tweets') }} AS r
          left join {{ ref('tweets_sources') }} AS t on r.user_id = t.user_id
          left join {{ ref('hashtag') }} AS h on r.user_id = h.user_id and r.tweet_date = h.tweet_date
          left join {{ ref('screen_name') }} l on l.user_id = r.user_id and l.tweet_date = r.tweet_date
-group by r.tweet_date, r.user_id, r.screen_name, h.hashtags, t.tweet_sources
+group by r.tweet_date, r.user_id, r.screen_name, h.hashtag, t.tweet_sources, l.screen_name
