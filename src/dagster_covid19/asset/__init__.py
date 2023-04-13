4f06e69945faf4f9b4b0ff8c66d0674611919106
Sotatek-HaiHo <hai.ho@sotatek>
2023-04-13 12:30:59 +0700

    [COVID19] Update

--- src/dagster_covid19/asset/__init__.py
+++ src/dagster_covid19/asset/__init__.py
@@ -1,5 +1,9 @@
 from dagster_dbt import load_assets_from_dbt_project
 from dagster import file_relative_path
+from dagster_dbt import dbt_cli_resource
+from src.dagster_covid19 import asset
+from src.dagster_covid19.asset.import_data import raw_tweets
+from dagster import Definitions, load_assets_from_modules
 
 DBT_PROJECT_PATH = file_relative_path(__file__, "../../../covid19")
 DBT_PROFILES = file_relative_path(__file__, "../../../.dbt")
@@ -7,3 +11,15 @@ DBT_PROFILES = file_relative_path(__file__, "../../../.dbt")
 dbt_assets = load_assets_from_dbt_project(
     project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["covid19"]
 )
+
+resources = {
+    "dbt": dbt_cli_resource.configured(
+        {
+            "project_dir": DBT_PROJECT_PATH,
+            "profiles_dir": DBT_PROFILES,
+        },
+    )
+}
+a = load_assets_from_modules([asset])[:]
+a.append(raw_tweets)
+defs = Definitions(assets=a, resources=resources)
