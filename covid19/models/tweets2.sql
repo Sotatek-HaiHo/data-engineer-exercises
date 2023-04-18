49cd37a636b8a21bef2ba1538d052ded2e71a426
Sotatek-HaiHo <hai.ho@sotatek>
2023-04-18 18:01:25 +0700

    refactor: tweets2.sql

--- covid19/models/tweets2.sql
+++ covid19/models/tweets2.sql
@@ -16,4 +16,3 @@ FROM {{ ref('reply_delay') }} AS t1
          LEFT JOIN
      {{ ref('tweet_number') }} AS t2
 ON t1.reply_date = t2.reply_date AND t1.original_user_id = t2.original_user_id AND t1.reply_user_id = t2.reply_user_id
-WHERE t1.is_quote = TRUE
