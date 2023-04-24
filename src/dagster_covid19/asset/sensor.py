952a90c97b4cbf01e175ba662b875650b8c7be4b
Kien Truong <kien.truong@sotatek.com>
2023-04-24 10:51:47 +0700

    fix: proper override of DBT_PROJECT_PATH and DBT_PROFILES

--- src/dagster_covid19/asset/sensor.py
+++ src/dagster_covid19/asset/sensor.py
@@ -11,15 +11,11 @@ from dagster import (
 )
 from dagster_dbt import load_assets_from_dbt_project
 
-DEFAULT_PROJECT_PATH = "../../../covid19"
-DEFAULT_PROFILES_PATH = "../../../.dbt"
+DEFAULT_PROJECT_PATH = file_relative_path(__file__, "../../../covid19")
+DEFAULT_PROFILES_PATH = file_relative_path(__file__, "../../../.dbt")
 
-DBT_PROJECT_PATH = file_relative_path(
-    __file__, os.getenv("DBT_PROJECT_PATH", DEFAULT_PROJECT_PATH)
-)
-DBT_PROFILES = file_relative_path(
-    __file__, os.getenv("DBT_PROFILES", DEFAULT_PROFILES_PATH)
-)
+DBT_PROJECT_PATH = os.getenv("DBT_PROJECT_PATH", DEFAULT_PROJECT_PATH)
+DBT_PROFILES = os.getenv("DBT_PROFILES", DEFAULT_PROFILES_PATH)
 
 dbt_assets = load_assets_from_dbt_project(
     project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["covid19"]
